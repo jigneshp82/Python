b8ba834039d5192f11e3128e7219d9957fb1960d
Jignesh Pandya <jigs.bagy@gmail.com>
2022-01-16 20:34:10 -0500

    Algo practice

--- Algo/maxDistToClosest.py
+++ Algo/maxDistToClosest.py
@@ -1,9 +1,28 @@
+from fcntl import DN_DELETE
+from math import ceil
+import re
+
+
 class Solution:
     def maxDistToClosest(self, seats: list) -> int:
-        pass
+        l = len(seats)
+        dist = seats.index(1)
+        res = 0
+        for s in seats:
+            if s:
+                res = max(res, ceil(dist/2))
+                dist = 0
+            else:
+                dist +=1
+        return max(dist, res)
+
+            
+
+
+
 
 
 
 s = Solution()
-seats = [1,0,0,0,1,0,1]
-s.maxDistToClosest(seats)
+seats = [0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0]
+print(s.maxDistToClosest(seats))
