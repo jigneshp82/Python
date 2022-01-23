766b5eb2a4c5ba7e9a8c0bf1006ea2b4eb334e92
Jignesh Pandya <jigs.bagy@gmail.com>
2022-01-23 17:04:49 -0500

    sequentialDigits

--- Algo/sequentialDigits.py
+++ Algo/sequentialDigits.py
@@ -1,3 +1,31 @@
+from collections import deque
+
+
 class Solution:
-    def sequentialDigits(self, low: int, high: int) -> List[int]:
+    def sequentialDigits(self, low: int, high: int) -> list:
+        l1 = len(str(low))
+        l2 = len(str(high))
+        ans = []
+        s = '123456789'
+        while l1 <= l2:
+            for x in range(len(s) -l1 +1):
+                num = int(s[x:x+l1])
+                if low <= num <= high:
+                    ans.append(num)
+            l1 +=1
+        return(ans)
         
+            
+         
+
+
+s = Solution()
+l = 10
+h = 467
+print(s.sequentialDigits(l,h))
+
+q =deque(range(1,10))
+print (q)
+while q:
+    c = q.popleft()
+    print(c)
